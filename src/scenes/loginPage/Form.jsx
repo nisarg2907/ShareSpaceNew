cf84478e9ed9a38385075c9428f75861f3923e13
Nisarg <nisargshah.ict19@gmail.com>
2023-05-19 22:59:41 +0530

    fixed

--- src/scenes/loginPage/Form.jsx
+++ src/scenes/loginPage/Form.jsx
@@ -65,7 +65,7 @@ const registerSchema = yup.object().shape({
       formData.append("picturePath", values.picture.name);
   
       const savedUserResponse = await fetch(
-        "http://localhost:3001/auth/register",
+        "https://sharespace-wquo.onrender.com/auth/register",
         {
           method: "POST",
           body: formData,
@@ -80,7 +80,7 @@ const registerSchema = yup.object().shape({
     };
   
     const login = async (values, onSubmitProps) => {
-      const loggedInResponse = await fetch("http://localhost:3001/auth/login", {
+      const loggedInResponse = await fetch("https://sharespace-wquo.onrender.com/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(values),
